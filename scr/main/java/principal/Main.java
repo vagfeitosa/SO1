3351be97a9bc5e2004cccfd82aa519f2bcaac5ce
Vagner Feitosa <70916096+vagfeitosa@users.noreply.github.com>
2023-12-06 11:15:22 -0300

    Create Main.java

---
+++ scr/main/java/principal/Main.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        ControleReceitas controlador = new ControleReceitas();
+
+        controlador.exibirItaliano("C:\\TEMP\\marg.json");
+    }
+}
